f8782193383e7b7d88b7ac4904cc138e5fe5fe9b
ANAR <qarabala4732@gmail.com>
2023-12-10 06:00:39 +0400

    fixed f3+d

--- src/main/java/net/fabricmc/dcch/mixin/DCCHMixin.java
+++ src/main/java/net/fabricmc/dcch/mixin/DCCHMixin.java
@@ -2,15 +2,16 @@ package net.fabricmc.dcch.mixin;
 
 import net.minecraft.client.gui.hud.ChatHud;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatHud.class)
 public class DCCHMixin {
-	/**
-	 * @author anar4732
-	 */
-	@Overwrite
-	public void clear(boolean clearHistory) {
-		// Do not clear the chat history
+	@Inject(at = @At("HEAD"), method = "clear", cancellable = true)
+	public void clear(boolean clearHistory, CallbackInfo ci) {
+		if (clearHistory) {
+			ci.cancel();
+		}
 	}
 }
